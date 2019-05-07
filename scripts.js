d8a31c48d3224a0163b5b2d87faddaf04ec72351
David Ortiz <David.Ortiz@sluhn.org>
2019-05-07 14:23:31 -0400

    refactor code

--- scripts.js
+++ scripts.js
@@ -1,35 +1,34 @@
-function checkDevice(){
-    var parser = new UAParser();
 
+function fetchDeviceInfo(){
+    var parser = new UAParser();
     var result = parser.getResult();
     
-    
-    var infoDiv = document.getElementById('info');
+    parseDeviceInfo(result);
+}
 
+function parseDeviceInfo(result){
+
+    var infoDiv = document.getElementById('info');
     var osDiv = document.getElementById('os');
     var versionDiv = document.getElementById('version');
     var deviceDiv = document.getElementById('device');
     var deviceRow = document.getElementById('deviceRow');
     
-
     osDiv.value = result.os.name;
     versionDiv.value = result.os.version;
 
     if(result.device.vendor){
-        deviceDiv.value = capitalize(result.device.type)  +" - "+ result.device.vendor +" "+ result.device.model ;
+        deviceDiv.value = result.device.vendor +" "+ result.device.model ;
         deviceRow.style.display = '';
     } else {
         deviceRow.style.display = 'none';
     }
     
-
     infoDiv.style.display = '';
-
 }
 
-function capitalize(s)
-{
-    //return s;
-    return s[0].toUpperCase() + s.slice(1);
+function init(){
+    fetchDeviceInfo();
 }
 
+window.onload = init;
